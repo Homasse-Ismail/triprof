a21174fed7ff6feac95576cb9d1fad65913c4547
HomasseIsmail <homasseismail@gmail.com>
2025-06-29 02:31:31 +0100

    aja

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const EtudiantRoutes = require('../Routes/etudiants');
+const EtudiantRoutes = require('./Routes/etudiants');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// ربط MongoDB باستخدام متغير بيئي
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 
-// routes
 app.use('/api/etudiants', EtudiantRoutes);
 
 module.exports = serverless(app);
---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const app = express();
+const PORT = 6000;
+
+app.route("/",(req,res)=>{
+    res.send('hello test')
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
