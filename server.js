5fbce5fbe46314328acdc42edcc8294e2a64dd03
HomasseIsmail <homasseismail@gmail.com>
2025-06-29 02:41:35 +0100

    ff

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-
 const express = require('express');
 const app = express();
 const PORT = 6000;
 
-app.route("/t",(req,res)=>{
-    res.send('hello test')
-})
-app.listen(PORT,()=>{
+// Correction ici : app.get au lieu de app.route
+app.get("/t", (req, res) => {
+    res.send('hello test');
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
